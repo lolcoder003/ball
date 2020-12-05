f602ff2752c1dd750981f5b9cedc8217563279fb
lolcoder003 <74305446+lolcoder003@users.noreply.github.com>
2020-12-05 11:06:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var side1, side2, side3;
+var ball;
+var ground;
+var world;
+
+function setup() {
+	createCanvas(1250, 650);
+	rectMode(CENTER);
+
+
+	engine = Engine.create();
+	world = engine.world;
+	
+	ball = new Ball(200,450,70);
+	ground = new Ground(width/2,620,width,20);
+	side1 = new Side(1200,510,20,200);
+	side2 = new Side(1000,510,20,200);
+	side3 = new Side(1100,600,200,20);
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(230);
+ 
+  
+  ball.display();
+  ground.display();
+  side1.display();
+  side2.display();
+  side3.display();
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+
+	  Matter.Body.applyForce(ball.body,ball.body.position,{x:130,y:-145});
+
+  
+	}
+}
+
+
+
